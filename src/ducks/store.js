72be2023995f9d77fabf637c6922bf76d3409dd3
Mars_Clarke <marsthecoder@outlook.com>
2020-12-10 14:14:00 -0700

    trying out redux dev tools

--- src/ducks/store.js
+++ src/ducks/store.js
@@ -1,1 +1,12 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
+import promiseMiddleware from "redux-promise-middleware";
+import budgetReducer from "./budgetReducer";
+
+const rootReducer = combineReducers({
+  budget: budgetReducer,
+});
+
+export default createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(promiseMiddleware))
+);
