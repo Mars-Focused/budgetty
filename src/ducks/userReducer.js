91ef7537f5aa3c4b06d8be37bfbe5f63c87c30dc
Mars_Clarke <marsthecoder@outlook.com>
2020-12-10 15:03:23 -0700

    having trouble on #5

--- src/ducks/userReducer.js
+++ src/ducks/userReducer.js
@@ -1,9 +1,18 @@
+import axios from "axios";
+
 const initialState = {
   email: null,
   firstName: null,
   lastname: null,
 };
 
+const REQUEST_USER_DATA = "REQUEST_USER_DATA";
+
+export function requestUserData = () => {
+    data = axios.get()
+}
+
 export default function reducer(state = initialState, action) {
   return state;
+  
 }
